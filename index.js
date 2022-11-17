26e1cefbe3c15ee51030b68bab8f00bc3de3fad3
Fangfang <fxiao.mecheng@gmail.com>
2022-11-17 16:01:26 +0000

    file system: unlink

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-fs.writeFile('mynewfile3.txt', 'This is my text', (err) => {
-  if (err) throw err;
-  console.log('Replaced!')
+fs.unlink('mynewfile2.txt', err => {
+  if(err) throw err;
+  console.log('File deleted!')
 })
-

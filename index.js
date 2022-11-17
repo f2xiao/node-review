ae0fac35a97a094ab8508773ae4a00ddd9372577
Fangfang <fxiao.mecheng@gmail.com>
2022-11-17 15:19:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require('http');
+const url = require('url');
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type':'text/html'});
+  const q = url.parse(req.url, true).query;
+  const txt = q.year + " " + q.month
+  res.end(txt);
+}).listen(8000)
+

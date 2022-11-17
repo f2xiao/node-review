4a122c5aeef6b86baf7f5da02d49162604f72b0a
Fangfang <fxiao.mecheng@gmail.com>
2022-11-17 15:59:36 +0000

    file system: appendFile, open, writeFile

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var http = require('http');
-const url = require('url');
-http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type':'text/html'});
-  const q = url.parse(req.url, true).query;
-  const txt = q.year + " " + q.month
-  res.end(txt);
-}).listen(8000)
+const fs = require('fs');
+fs.writeFile('mynewfile3.txt', 'This is my text', (err) => {
+  if (err) throw err;
+  console.log('Replaced!')
+})
 
